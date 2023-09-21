068bc737d4b651eb3fa108de2f72725b70bbee1b
Rennssee <djwad17@gmail.com>
2023-09-21 19:53:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-name = input(">>> ")
-age = input("Age>>> ")
-print("Hello " + name + " " + age)
+# message = "Never argue with stupid people, they will drag you down to their level and then beat you with experience."
+# search = "r"
+# result = 0
+# for char in message:
+#    if char == search:
+#        result = result + 1
+# print(result)
+num = int(input("Enter integer (0 for output): "))
+sum = 0
+while num > 0:
+    for i in range(num + 1)5:
+        sum = sum + i
+    print(sum)
+    num = int(input("Enter integer (0 for output): "))
