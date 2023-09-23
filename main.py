f23c929a2c060517b91e1382d87d65e47fef2d1e
Rennssee <djwad17@gmail.com>
2023-09-23 22:59:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@
 num = int(input("Enter integer (0 for output): "))
 sum = 0
 while num > 0:
-    for i in range(num + 1)5:
+    for i in range(num + 1):
         sum = sum + i
     print(sum)
     num = int(input("Enter integer (0 for output): "))
