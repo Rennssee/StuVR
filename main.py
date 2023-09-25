e4189f78e7b55670d1e6957238e1f1bb26860f61
Rennssee <djwad17@gmail.com>
2023-09-25 16:46:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-# message = "Never argue with stupid people, they will drag you down to their level and then beat you with experience."
-# search = "r"
-# result = 0
-# for char in message:
-#    if char == search:
-#        result = result + 1
-# print(result)
-num = int(input("Enter integer (0 for output): "))
-sum = 0
-while num > 0:
-    for i in range(num + 1):
-        sum = sum + i
-    print(sum)
-    num = int(input("Enter integer (0 for output): "))
+base_rate = 40
+price_per_km = 10
+total_trip = 0
+
+
+def calculate_trip_price(distance_km):
+    global total_trip
+    total_trip += 1
+    return (distance_km * price_per_km) + base_rate
+
+
+print(calculate_trip_price(float(input(">>"))))
+print(total_trip)
